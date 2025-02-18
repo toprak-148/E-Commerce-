60c4045e3cb92645f695c87f5b2c2e1863def3c6
Toprak Dogan <toprakdgn148@gmail.com>
2025-02-19 02:33:38 +0300

    add ProductRepository findByNameContaining method and create component:search

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/dao/ProductRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -16,9 +15,15 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
     // will perform a search based on category id
-    // DB : select * from product where category_id=?
+    // similar DB : select * from product where category_id=?
     //localhost:8080/api/products/findByCategoryId?id=1
-
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+
+    // will perform a search on product:name
+    // similar DB : select * from product p where p.name like concat('%',:name,'%')
+    // localhost:8080/api/products/search/findByNameContaining?name=?
+    Page<Product> findByNameContaining(@Param("name") String name , Pageable pageable);
+
 
 }
