2829c7091c4a6469c1635b44aa55d36b87a53ed8
Toprak Dogan <toprakdgn148@gmail.com>
2025-02-24 01:59:12 +0300

    Shopping Cart Status update Cart Details add Cart Status

--- 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -14,7 +16,7 @@ export class ProductDetailsComponent implements OnInit {
   product:Product;
 
 
-  constructor(private route:ActivatedRoute,private productService:ProductService) { }
+  constructor(private route:ActivatedRoute,private productService:ProductService,private cartService:CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -34,5 +36,11 @@ export class ProductDetailsComponent implements OnInit {
       }
     )
   }
+  addToCart()
+  {
+    console.log(`adding to cart :${this.product.name} , ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
 
+  }
 }
