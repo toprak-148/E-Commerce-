dc1e7f004ca916d4cb94e7ec5f7d97631bfdd550
Toprak Dogan <toprakdgn148@gmail.com>
2025-02-26 02:47:32 +0300

    Back-end Configuration ! - Mapped by country , state ManyToOne and OneToMany mapping

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/entity/Country.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/entity/Country.java
@@ -1,10 +1,38 @@
 package com.td005.spring_ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "country")
+@Getter
+@Setter
+
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name ="code" )
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+
+    // TODO : Set up one-to-many with states
+    @OneToMany(mappedBy = "country")
+     private List<State> states;
     
+
+
+
+
+
 }
