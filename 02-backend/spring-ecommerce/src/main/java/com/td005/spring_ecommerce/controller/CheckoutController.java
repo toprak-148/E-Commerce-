ef71d19258a3c75c10a8df4973060d2ae0c2e107
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-04 04:52:36 +0300

    Save the Order Db - Create Rest Controller

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/controller/CheckoutController.java
@@ -1,5 +1,29 @@
 package com.td005.spring_ecommerce.controller;
 
+import com.td005.spring_ecommerce.dto.Purchase;
+import com.td005.spring_ecommerce.dto.PurchaseResponse;
+import com.td005.spring_ecommerce.service.CheckoutService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@RequestMapping("/api/checkout")
 public class CheckoutController {
-    
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService)
+    {
+        this.checkoutService = checkoutService;
+    }
+
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase)
+    {
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
 }
