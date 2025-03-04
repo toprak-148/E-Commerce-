6d2087f2cfbd484ca9414de6986e55f4094df349
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-04 05:39:51 +0300

    CheckoutForm - Save the Order - Frontend - Purchase

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { start } from 'repl';
-import { NEVER, never, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Country } from 'src/app/common/country';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
 import { FormService } from 'src/app/services/form.service';
 import { ShopValidators } from 'src/app/validators/shop-validators';
 
@@ -31,7 +35,11 @@ export class CheckoutComponent implements OnInit {
 
 
 
-  constructor(private formBuilder:FormBuilder,private formService:FormService,private cartService:CartService) { }
+  constructor(private formBuilder:FormBuilder,
+              private formService:FormService,
+              private cartService:CartService,
+              private checkoutService:CheckoutService,
+              private router : Router) { }
 
   ngOnInit(): void {
 
@@ -108,6 +116,9 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
+    this.checkoutService.placeOrder().subscribe();
+
+
 
 
 
@@ -164,16 +175,61 @@ export class CheckoutComponent implements OnInit {
 
     onSubmit()
     {
+      console.log('Handling the submit button');
+
       if(this.checkoutFormGroup.invalid)
       {
         this.checkoutFormGroup.markAllAsTouched();
+        return;
       }
 
-      console.log('Handling the submit button');
-      console.log(this.checkoutFormGroup.get('customer')?.value);
-      console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
-      console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress')?.value.country.name);
-      console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress')?.value.state.name);
+      //* set up order
+      let order = new Order();
+      order.totalPrice = this.totalPrice;
+      order.totalQuantity = this.totalQuantity;
+
+
+      //* get cart items
+      const cartItems = this.cartService.cartItems;
+
+
+
+      //* create orderItems from cartItems
+      const orderItems : OrderItem[] = [];
+      //* for(let i = 0 ;i < cartItems.length;i++)
+      //*   orderItems[i] = new OrderItem(cartItems[i]);
+      // daha kısa bir yolu var
+      let orderItemsShort:OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+
+
+      //* set up purchase
+      let purchase:Purchase = new Purchase();
+
+
+      //* populate purchase - customer
+      purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+
+      //* populate purchase - shipping address
+      purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+      const shippingState:State = JSON.parse(JSON.stringify(purchase.shippingAddress.state));
+      const shippingCountry:Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+      purchase.shippingAddress.state = shippingState.name;
+      purchase.shippingAddress.country = shippingCountry.name;
+
+      //* populate purchase - billing address
+      purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+      const billingState:State = JSON.parse(JSON.stringify(purchase.billingAddress.state));
+      const billingCountry:Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+      purchase.billingAddress.state = billingState.name;
+      purchase.billingAddress.country = billingCountry.name;
+
+      //* populate purchase - order and orderItems
+
+      //* call REST API via the CheckoutService
+
+
+
 
     }
 
