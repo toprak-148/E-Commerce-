17c45624d85ae01faf022a3e8a5bfd1f57d6abbd
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-04 04:20:54 +0300

    New Repository - Customer Repository

---
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/dao/CustomerRepository.java
@@ -0,0 +1,11 @@
+package com.td005.spring_ecommerce.dao;
+
+import com.td005.spring_ecommerce.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+//customer kullanılmasının nedeni, müşteri bir sipariş koleksiyonu vardır.Yani bir satın alma gerçekleştiğinde müşteriyi yakalayacağız,
+// uygun eşleşmede gerçek müşteriyi bulacağız.
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    
+}
