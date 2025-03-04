bce08bfb4e96b5a36a8fdd086e402cf010973213
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-04 05:46:05 +0300

    CheckoutForm - Save The order - Frontend - Call Service

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -116,7 +116,7 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
-    this.checkoutService.placeOrder().subscribe();
+
 
 
 
@@ -225,12 +225,42 @@ export class CheckoutComponent implements OnInit {
       purchase.billingAddress.country = billingCountry.name;
 
       //* populate purchase - order and orderItems
+      purchase.order = order;
+      purchase.orderItem = orderItems;
 
       //* call REST API via the CheckoutService
+      this.checkoutService.placeOrder(purchase).subscribe(
+        {
+          next:response=>{
+            alert(`Your order has been received\nOrder tracking number: ${response.orderTrackingNumber}`);
+            // reset cart
+            this.resetCart();
+
+          },
+          error: err =>{
+            alert(`There was an error:${err.message}`);
+          }
+        }
+      )
 
 
 
 
+    }
+
+
+    resetCart(){
+      // reset cart data
+      this.cartService.cartItems = [];
+      this.cartService.totalPrice.next(0);
+      this.cartService.totalQuantity.next(0);
+
+
+      // reset the form
+      this.checkoutFormGroup.reset();
+
+      // navigate back to the products page
+      this.router.navigateByUrl("/products")
     }
 
 
