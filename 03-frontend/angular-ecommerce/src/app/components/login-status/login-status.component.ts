c28b527c646fb7e5dafd029d61f2f69127e3afb7
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-05 05:12:29 +0300

    Create the Login Component

--- 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import OktaSingIn from '@okta/okta-signin-widget';
+import myAppConfig from 'src/app/config/my-app-config';
 @Component({
   selector: 'app-login-status',
   templateUrl: './login-status.component.html',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginStatusComponent implements OnInit {
 
-  constructor() { }
+
+  oktaSignIn:any;
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth:OktaAuth) {
+    this.oktaSignIn = new this.oktaSignIn({
+      logo:'../../../favicon.ico',
+      baseUrl:myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId:myAppConfig.oidc.clientId,
+      redirectUri:myAppConfig.oidc.redirectUri,
+      authParams:{
+        pkce:true,
+        issuer:myAppConfig.oidc.issuer,
+        scopes:myAppConfig.oidc.scopes
+      }
+    });
+
+
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
