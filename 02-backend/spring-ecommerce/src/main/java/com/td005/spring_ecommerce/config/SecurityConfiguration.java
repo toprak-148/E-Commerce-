44c528559f30ad8329c81a6ec8f2676303a3cee4
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-08 00:36:34 +0300

    Secure:Order History -Secure Backend - Protect Endpoints

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/config/SecurityConfiguration.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/config/SecurityConfiguration.java
@@ -1,4 +1,44 @@
 package com.td005.spring_ecommerce.config;
 
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
+
+        // protect endpoint / api/orders
+        //protect endpoint /api/orders
+        http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
+        // + CORS FİLTERS
+        http.cors(Customizer.withDefaults());
+
+        // + content negotitation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,new HeaderContentNegotiationStrategy());
+
+        // + non-empty response body for 401 (more friendly)
+        Okta.configureResourceServer401ResponseBody(http);
+
+        //we are not using cookies for sessiın tracking >> disable CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+
+
+    }
+
 }
