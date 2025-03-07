ce2ec6d89d454fd97c25b76ffa335ac631233e80
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-07 20:59:42 +0300

    Order History - Frontend: Create OrderHistoryService

---
+++ 03-frontend/angular-ecommerce/src/app/services/order-history.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrderHistory } from '../common/order-history';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = 'htttp://localhost:8080/api/orders';
+
+
+  constructor(private http:HttpClient) {
+
+
+
+   }
+
+   getOrderHistory(theEmail:string):Observable<GetResponseOrderHistory>
+   {
+
+    // need to build URL based on the customer email
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmail?email=${theEmail}`;
+    return this.http.get<GetResponseOrderHistory>(orderHistoryUrl);
+
+
+   }
+}
+
+
+
+interface GetResponseOrderHistory{
+
+  _embedded:{
+    orders:OrderHistory[];
+  }
+}
