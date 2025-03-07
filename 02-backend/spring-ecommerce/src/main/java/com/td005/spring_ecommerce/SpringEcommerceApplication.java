7893b2c11ab6d9ffa3de98e1ed985f7b1d299c37
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-07 20:53:12 +0300

    OrderHistory -Frontend : Track User's email

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/SpringEcommerceApplication.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/SpringEcommerceApplication.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringEcommerceApplication {
 	public static void main(String[] args){SpringApplication.run(SpringEcommerceApplication.class, args);}
-
-
-
+	
 }
 
 
--- 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/login-status/login-status.component.ts
@@ -12,6 +12,9 @@ export class LoginStatusComponent implements OnInit {
   isAuthenticated:boolean = false;
   userFullName:string='';
 
+  //I used sessionStorage to keep the order history from disappearing.
+  storage : Storage = sessionStorage;
+
 
   constructor(private oktaAuthService:OktaAuthStateService , @Inject(OKTA_AUTH) private oktaAuth : OktaAuth) {
 
@@ -37,6 +40,11 @@ export class LoginStatusComponent implements OnInit {
       this.oktaAuth.getUser().then(
         (res)=>{
           this.userFullName = res.name as string;
+          //retrieve the users's email from authentication response
+          const theEmail = res.email;
+
+          //now storage the email in browser storage
+          this.storage.setItem('userEmail',JSON.stringify(theEmail));
         }
       )
     }
@@ -49,5 +57,9 @@ export class LoginStatusComponent implements OnInit {
   this.oktaAuth.signOut();
 
   }
+
+
+
+
 
 }
