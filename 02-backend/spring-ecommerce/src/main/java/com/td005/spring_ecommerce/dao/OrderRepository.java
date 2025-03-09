30890234c2ceb41dd9aff0ed28abb38e3c3a94cc
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-09 23:47:14 +0300

    backend - frontend general configuration

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/dao/OrderRepository.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/dao/OrderRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
 
