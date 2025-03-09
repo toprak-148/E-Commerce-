78bf9e6918f24fdf5332bdae723c1bd5088529cb
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-10 00:23:31 +0300

    Secure Communication with HTTPS - spring boot backend

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,12 +1,13 @@
 export default{
   oidc:{
+    //Okta authentication yapılanması
     // istemci uygulaması genel adı : clientId
     //issuer:belirteçleri verendir kullanılan url bu okta yetkilendirmesi sunucusuyla teki verir.
     //redirectUri:temel yönlendirme kullanıcı oturum açtıktan sonra belirtilen yere gönderilir.
     // scopes: kullanici hakkinda bilgi almak icin
     clientId:'0oanocrbv5tkctgvk5d7',
     issuer:'https://dev-16920228.okta.com/oauth2/default',
-    redirectUri:'http://localhost:4200/login/callback',
+    redirectUri:'https://localhost:4200/login/callback',
     scopes:['openid','profile','email']
   }
 
