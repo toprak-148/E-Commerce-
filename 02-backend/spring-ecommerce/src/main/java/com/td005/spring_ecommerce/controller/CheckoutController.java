d4ead9b39fa522469db19ed559bd4796976dd421
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-10 20:43:22 +0300

    Payment Processing : Stripe - configuration checkoutController class

--- 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-ecommerce/src/main/java/com/td005/spring_ecommerce/controller/CheckoutController.java
@@ -1,8 +1,13 @@
 package com.td005.spring_ecommerce.controller;
 
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.td005.spring_ecommerce.dto.PaymentInfo;
 import com.td005.spring_ecommerce.dto.Purchase;
 import com.td005.spring_ecommerce.dto.PurchaseResponse;
 import com.td005.spring_ecommerce.service.CheckoutService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,4 +29,16 @@ public class CheckoutController {
 
         return purchaseResponse;
     }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException
+    {
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+
+    }
+
 }
