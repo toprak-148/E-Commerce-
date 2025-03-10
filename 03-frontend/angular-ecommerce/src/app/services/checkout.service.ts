a20d8f4163510cfcecf7fb2ab53beb8e89ff3fa2
Toprak Dogan <toprakdgn148@gmail.com>
2025-03-10 22:18:44 +0300

    Payment Processing - Frontend : İnstall Stripe API and create a typescript declaration file, add stripe publisable key

--- 03-frontend/angular-ecommerce/src/app/services/checkout.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +12,19 @@ export class CheckoutService {
 
 
   private purchaseUrl:string = environment.shopAppUrl +"/checkout/purchase";
+  private paymentIntentUrl = environment.stripePublishableKey + "/checkout/payment-intent";
 
   constructor(private httpClient:HttpClient) { }
 
 
   placeOrder(purchase:Purchase):Observable<any>{
     return this.httpClient.post<Purchase>(this.purchaseUrl,purchase);
+
+  }
+
+
+  createPaymentIntent(paymentInfo:PaymentInfo):Observable<any>
+  {
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl,paymentInfo);
   }
 }
